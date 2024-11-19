72ee2e2232c32c4faff347e99b7dd6d5c34e6b59
David Zhu <davidyzhu2@gmail.com>
2024-11-19 10:45:06 -0800

    Add basic calculator layout
    Created basic calculator UI in HTML/CSS, with color and alignment.
    Functionality not yet added.

---
+++ scripts.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+    return Number(a) - Number(b);
+}
+
+function multiply(a, b) {
+    return Number(a) * Number(b);
+}
+
+function divide(a, b) {
+    return Number(a) / Number(b)
+}
+
+let firstOperand;
+let operator;
+let secondOperand;
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1,num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "ERROR";
+    }
+}
