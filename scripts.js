2679c7c77c2f8ffa4e17a61930a40bd1d78269e2
David Zhu <davidyzhu2@gmail.com>
2024-11-19 16:51:31 -0800

    Add percentage and sign features, limit length
    Prevented user from entering numbers past display length. Truncated decimals,
    returned error for overflows.

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return Number(a) / Number(b)
+    return Number(a) / Number(b);
 }
 
 let firstOperand;
@@ -19,28 +19,51 @@ let operator;
 let secondOperand;
 
 function operate(num1, operator, num2) {
+    let ret;
     switch (operator) {
         case "+":
-            return add(num1,num2);
+            ret = add(num1,num2);
+            break;
         case "-":
-            return subtract(num1, num2);
+            ret =  subtract(num1, num2);
+            break;
         case "*":
-            return multiply(num1, num2);
+            ret = multiply(num1, num2);
+            break;
         case "/":
-            return divide(num1, num2);
+            if (num2 == 0) {
+                return "ERROR";
+            }
+            else ret = divide(num1, num2);
+            break;
         default:
             return "ERROR";
     }
+    if (!String(ret).includes('.') && String(ret).length > 8) return "ERROR";
+    return String(ret).slice(0, 8);
 }
 
-let display = '';
 const displayElement = document.querySelector(".calculator-display");
+beginNewDisplay = true;
+
+function resetAll() {
+    firstOperand = null;
+    secondOperand = null;
+    operator = null;
+
+    beginNewDisplay = true;
+}
 
 const operandsList = Array.from(document.querySelectorAll(".operands > div > button"));
 operandsList.forEach((element) => {
     element.addEventListener("click", () => {
-        display += element.textContent;
-        displayElement.textContent = display;
+        let currentDisplay = displayElement.textContent;
+        if (currentDisplay == '0' && element.textContent == 0) return;
+        if (beginNewDisplay || currentDisplay == 'ERROR') {
+            currentDisplay = '';
+            beginNewDisplay = false;
+        }
+        displayElement.textContent = (currentDisplay + element.textContent).slice(0,8);
     })
 })
 
@@ -48,19 +71,20 @@ const operatorList = Array.from(document.querySelectorAll(".operators > button")
 operatorList.splice(4,1);
 operatorList.forEach((element) => {
     element.addEventListener("click", () => {
+        if (displayElement.textContent == "ERROR") return;
         if (!firstOperand) {
-            firstOperand = display;
-            display = '';
-            operator = element.textContent;
-        } else {
-            secondOperand = display;
-            display = '';
-            let result = operate(firstOperand, operator, secondOperand)
+            firstOperand = displayElement.textContent;
+        } 
+        else {
+            secondOperand = displayElement.textContent;
+            let result = operate(firstOperand, operator, secondOperand);
             displayElement.textContent = result;
+
             firstOperand = result;
             secondOperand = null;
-            operator = element.textContent;
         }
+        operator = element.textContent;
+        beginNewDisplay = true;
     })
 })
 
@@ -68,19 +92,27 @@ const equalElement = document.querySelector(".equal");
 equalElement.addEventListener("click", () => {
     if (!operator) return;
 
-    secondOperand = display;
-    display = '';
+    secondOperand = displayElement.textContent;
     let result = operate(firstOperand, operator, secondOperand)
     displayElement.textContent = result;
-    firstOperand = result;
-    secondOperand = null;
+
+    resetAll();
 })
 
 const clearElement = document.querySelector(".calculator-clear");
 clearElement.addEventListener("click", () => {
-    display = '';
-    displayElement.textContent = 0;
-    firstOperand = null;
-    secondOperand = null;
-    operator = null;
+    resetAll();
+    displayElement.textContent = '0';
+})
+
+const signElement = document.querySelector(".calculator-sign");
+signElement.addEventListener("click", () => {
+    if (displayElement.textContent == '0' || displayElement.textContent == 'ERROR') return;
+    displayElement.textContent = -displayElement.textContent;
+})
+
+const percentElement = document.querySelector(".calculator-percent");
+percentElement.addEventListener("click", () => {
+    if (displayElement.textContent == "ERROR") return;
+    displayElement.textContent = divide(displayElement.textContent, 100);
 })
