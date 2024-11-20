9a4c3a45ca2b94e12ab638f1ae04572abd939fea
David Zhu <davidyzhu2@gmail.com>
2024-11-19 16:12:39 -0800

    Add basic functionality for buttons
    Implemented display, number buttons, operation buttons, and clear button.
    Allow chaining operations.

--- scripts.js
+++ scripts.js
@@ -32,3 +32,55 @@ function operate(num1, operator, num2) {
             return "ERROR";
     }
 }
+
+let display = '';
+const displayElement = document.querySelector(".calculator-display");
+
+const operandsList = Array.from(document.querySelectorAll(".operands > div > button"));
+operandsList.forEach((element) => {
+    element.addEventListener("click", () => {
+        display += element.textContent;
+        displayElement.textContent = display;
+    })
+})
+
+const operatorList = Array.from(document.querySelectorAll(".operators > button"));
+operatorList.splice(4,1);
+operatorList.forEach((element) => {
+    element.addEventListener("click", () => {
+        if (!firstOperand) {
+            firstOperand = display;
+            display = '';
+            operator = element.textContent;
+        } else {
+            secondOperand = display;
+            display = '';
+            let result = operate(firstOperand, operator, secondOperand)
+            displayElement.textContent = result;
+            firstOperand = result;
+            secondOperand = null;
+            operator = element.textContent;
+        }
+    })
+})
+
+const equalElement = document.querySelector(".equal");
+equalElement.addEventListener("click", () => {
+    if (!operator) return;
+
+    secondOperand = display;
+    display = '';
+    let result = operate(firstOperand, operator, secondOperand)
+    displayElement.textContent = result;
+    firstOperand = result;
+    secondOperand = null;
+})
+
+const clearElement = document.querySelector(".calculator-clear");
+clearElement.addEventListener("click", () => {
+    display = '';
+    displayElement.textContent = 0;
+    firstOperand = null;
+    secondOperand = null;
+    operator = null;
+})
